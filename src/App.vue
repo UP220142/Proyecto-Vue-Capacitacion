b6f192508e30725625b6866172c795a4bf765002
Vegetta777 <up220142@alumnos.upa.edu.mx>
2026-01-30 12:58:50 -0600

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ButtonCounter from './components/ButtonCounter.vue'
+</script>
+
+<template>
+  <div class="container">
+    <h1>APP</h1>
+    <ButtonCounter />
+    
+  </div>
+</template>
+
---
+++ src/components/BlogPost.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">Post 01</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nihil magni deleniti ratione pariatur hic et eligendi minima, incidunt repudiandae, vitae veniam, iure rerum? Tempore architecto dolor nesciunt veritatis ducimus?</p>
+        </div>
+    </div>
+</template>
