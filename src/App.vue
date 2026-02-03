f295a3f96425c184666779f798dfa3c360f15a9e
Vegetta777 <up220142@alumnos.upa.edu.mx>
2026-02-03 11:50:02 -0600

    feat :sparkles: Boton con 'emit:' para favoritos

--- src/App.vue
+++ src/App.vue
@@ -9,14 +9,19 @@ const posts = ref([
   { id: 3, title: 'Post 3', body: 'description 3' },
   { id: 4, title: 'Post 4' },
 ]);
+
+const favorito = ref("");
+
+const cambiarFavorito = (title) => {
+  favorito.value = title;
+};
 </script>
 
 <template>
   <div class="container">
     <h1>APP</h1>
-    <ButtonCounter />
-    <button-counter></button-counter>
-
-    <BlogPost v-for="post in posts" :key="post.id" :title="post.title" :id="post.id" :body="post.body" />
+    <h2>Mi Post Favorito: {{ favorito }}</h2>
+    <BlogPost v-for="post in posts" :key="post.id" :title="post.title" :id="post.id" :body="post.body"
+      @cambiarFavorito="cambiarFavorito" />
   </div>
 </template>
--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -14,6 +14,8 @@ defineProps({
     <div class="card-body">
       <h5 class="card-title">{{ id }} - {{ title }}</h5>
       <p>{{ body }}</p>
+      <button @click="$emit('cambiarFavorito', title)" class="btn btn-outline-primary">
+        Favorito</button>
     </div>
   </div>
 </template>
