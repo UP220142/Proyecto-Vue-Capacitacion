21684a107f085d77bbfb887fb8d32f3d253b57cd
Vegetta777 <up220142@alumnos.upa.edu.mx>
2026-02-03 13:15:15 -0600

    feat :sparkles: Paginacion Pt1

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,24 @@ import BlogPost from './components/BlogPost.vue'
 import PaginatePost from './components/PaginatePost.vue'
 
 const posts = ref([]);
+const postXpage = 10
+const inicio = ref (0)
+const fin = ref (postXpage)
 
 const favorito = ref("");
 
 const cambiarFavorito = (title) => {
   favorito.value = title;
 };
+
+const next = () => {
+  inicio.value += postXpage
+  fin.value += postXpage
+}
+const prev = () => {
+  inicio.value -= postXpage
+  fin.value -= postXpage
+}
 
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((res) => res.json())
@@ -24,9 +36,9 @@ fetch("https://jsonplaceholder.typicode.com/posts")
     <h1>APP</h1>
     <h2>Mi Post Favorito: {{ favorito }}</h2>
 
-    <PaginatePost class="mb-2"/> 
+    <PaginatePost @next="next" @prev="prev" class="mb-2"/> 
 
-    <BlogPost v-for="post in posts.slice(0, 10)" :key="post.id" :title="post.title" :id="post.id" :body="post.body"
+    <BlogPost v-for="post in posts.slice(inicio, fin)" :key="post.id" :title="post.title" :id="post.id" :body="post.body"
       @cambiarFavoritoNombre="cambiarFavorito" class="mb-2"/>
   </div>
 </template>
--- src/components/PaginatePost.vue
+++ src/components/PaginatePost.vue
@@ -1,8 +1,20 @@
-<script></script>
+<script setup>
+const emit = defineEmits(['next', 'prev']);
+
+const activarSiguiente = () => {
+    console.log("Me diste el siguiente");
+    emit('next');
+};
+const activarAnterior = () => {
+    console.log("Me diste el anterior");
+    emit('prev');
+};
+</script>
+
 
 <template>
     <div class="btn-group" role="group" aria-label="Basic example">
-        <button type="button" class="btn btn-outline-secondary">Previous</button>
-        <button type="button" class="btn btn-secondary">Next</button>
+        <button @click="activarAnterior" type="button" class="btn btn-outline-primary">Previous</button>
+        <button @click="activarSiguiente" type="button" class="btn btn-outline-primary">Next</button>
     </div>
 </template>
