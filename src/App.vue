4154b8e16dce66e04ec6304d3ce059e3060c6f94
Vegetta777 <up220142@alumnos.upa.edu.mx>
2026-02-03 10:25:37 -0600

    feat :sparkles: Creacion de multples props

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <script setup>
 import ButtonCounter from './components/ButtonCounter.vue'
+import BlogPost from './components/BlogPost.vue'
 </script>
 
 <template>
   <div class="container">
     <h1>APP</h1>
     <ButtonCounter />
+    <button-counter></button-counter>
     
+    <BlogPost title="Post 1" id="1" body="description 1" colorText="primary" />
+    <BlogPost title="Post 2" id="2" body="description 2" colorText="secondary"/>
+    <BlogPost title="Post 3" id="3" body="description 3" colorText="success"/>
+    <BlogPost title="Post 4" id="4" body="description 4" colorText="primary"/>
   </div>
 </template>
 
--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,10 +1,16 @@
-<script setup></script>
+<script setup>
+defineProps(["title", "id", "body ", "colorText"])
+</script>
+
 
 <template>
     <div class="card">
         <div class="card-body">
-            <h5 class="card-title">Post 01</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nihil magni deleniti ratione pariatur hic et eligendi minima, incidunt repudiandae, vitae veniam, iure rerum? Tempore architecto dolor nesciunt veritatis ducimus?</p>
+            <h5 
+                class="card-title" 
+                :class="`text-${colorText}`"
+            >{{ id }} - {{ title }} </h5>
+            <p>{{ body }}</p>
         </div>
     </div>
 </template>
