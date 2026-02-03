9dcc7090247c4f23e7d79a9aeaca889a565f7539
Vegetta777 <up220142@alumnos.upa.edu.mx>
2026-02-03 12:44:22 -0600

    feat :sparkles: Consumo de API y uso de slice

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <script setup>
-import ButtonCounter from './components/ButtonCounter.vue'
-import BlogPost from './components/BlogPost.vue'
 import { ref } from 'vue';
 
-const posts = ref([
-  { id: 1, title: 'Post 1', body: 'description 1' },
-  { id: 2, title: 'Post 2', body: 'description 2' },
-  { id: 3, title: 'Post 3', body: 'description 3' },
-  { id: 4, title: 'Post 4' },
-]);
+import BlogPost from './components/BlogPost.vue'
+import PaginatePost from './components/PaginatePost.vue'
+
+const posts = ref([]);
 
 const favorito = ref("");
 
 const cambiarFavorito = (title) => {
   favorito.value = title;
 };
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    posts.value = data;
+  });
 </script>
 
 <template>
   <div class="container">
     <h1>APP</h1>
     <h2>Mi Post Favorito: {{ favorito }}</h2>
-    
-    <BlogPost v-for="post in posts" :key="post.id" :title="post.title" :id="post.id" :body="post.body"
-      @cambiarFavoritoNombre="cambiarFavorito" />
+
+    <PaginatePost class="mb-2"/> 
+
+    <BlogPost v-for="post in posts.slice(0, 10)" :key="post.id" :title="post.title" :id="post.id" :body="post.body"
+      @cambiarFavoritoNombre="cambiarFavorito" class="mb-2"/>
   </div>
 </template>
---
+++ src/components/PaginatePost.vue
@@ -0,0 +1,8 @@
+<script></script>
+
+<template>
+    <div class="btn-group" role="group" aria-label="Basic example">
+        <button type="button" class="btn btn-outline-secondary">Previous</button>
+        <button type="button" class="btn btn-secondary">Next</button>
+    </div>
+</template>
