1e326300892d5badf1d26c37d90d40ad479595d4
Vegetta777 <up220142@alumnos.upa.edu.mx>
2026-02-04 10:46:42 -0600

    Feat :sparkles: Show functional spinner during fetch and hide on completion

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,13 @@ import { computed } from 'vue';
 
 import BlogPost from './components/BlogPost.vue'
 import PaginatePost from './components/PaginatePost.vue'
+import LoadingSpiner from './components/LoadingSpiner.vue'
 
 const posts = ref([]);
 const postXpage = 10;
 const inicio = ref(0)
 const fin = ref(postXpage)
+const loading = ref(true);
 
 const favorito = ref("");
 
@@ -31,17 +33,22 @@ fetch("https://jsonplaceholder.typicode.com/posts")
   .then((res) => res.json())
   .then((data) => {
     posts.value = data;
+
+  })
+  .finally(() => {
+    loading.value = false;
   });
 </script>
 
 <template>
-  <div class="container">
+  <LoadingSpiner v-if="loading" />
+  <div class="container" v-else>
     <h1>APP</h1>
     <h2>Mi Post Favorito: {{ favorito }}</h2>
 
-    <PaginatePost @next="next" @prev="prev" :inicio="inicio" :fin="fin" :totalPosts="totalPosts" class="mb-2"/> 
+    <PaginatePost @next="next" @prev="prev" :inicio="inicio" :fin="fin" :totalPosts="totalPosts" class="mb-2" />
 
-    <BlogPost v-for="post in posts.slice(inicio, fin)" :key="post.id" :title="post.title" :id="post.id" :body="post.body"
-      @cambiarFavoritoNombre="cambiarFavorito" class="mb-2"/>
+    <BlogPost v-for="post in posts.slice(inicio, fin)" :key="post.id" :title="post.title" :id="post.id"
+      :body="post.body" @cambiarFavoritoNombre="cambiarFavorito" class="mb-2" />
   </div>
 </template>
