bfd6960aeb2ecec0e14127a70bc09d11b763dc86
Vegetta777 <up220142@alumnos.upa.edu.mx>
2026-02-04 10:19:52 -0600

    Feat :sparkles: Pagination with validations and item count display

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import { computed } from 'vue';
 
 import BlogPost from './components/BlogPost.vue'
 import PaginatePost from './components/PaginatePost.vue'
@@ -10,6 +11,8 @@ const inicio = ref(0)
 const fin = ref(postXpage)
 
 const favorito = ref("");
+
+const totalPosts = computed(() => posts.value.length);
 
 const cambiarFavorito = (title) => {
   favorito.value = title;
@@ -36,7 +39,7 @@ fetch("https://jsonplaceholder.typicode.com/posts")
     <h1>APP</h1>
     <h2>Mi Post Favorito: {{ favorito }}</h2>
 
-    <PaginatePost @next="next" @prev="prev" class="mb-2"/> 
+    <PaginatePost @next="next" @prev="prev" :inicio="inicio" :fin="fin" :totalPosts="totalPosts" class="mb-2"/> 
 
     <BlogPost v-for="post in posts.slice(inicio, fin)" :key="post.id" :title="post.title" :id="post.id" :body="post.body"
       @cambiarFavoritoNombre="cambiarFavorito" class="mb-2"/>
